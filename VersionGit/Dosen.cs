5cc986e4a8ebd6db442f79a1da788726ac0cc73a
SilaCahyaDewi <130416431+SilaCahyaDewi@users.noreply.github.com>
2023-04-11 08:26:38 +0700

    Add files via upload

---
+++ VersionGit/Dosen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionGit
+{
+    class Dosen
+    {
+        private int id, Nik;
+        private string nama, gender, course;
+
+        public Dosen ()
+        {
+            id = 1;
+            Nik = 12345678;
+            nama = "Dosen Pengampu";
+            gender = "Male";
+            course = "Pemrogramanan Berorientasi Objek";
+        }
+
+        public void attDosen (string name, int nik)
+        {
+            nama = name;
+            Nik = nik;
+        }
+
+        public void addCourse (string newCourse)
+        {
+            course += " ," + newCourse;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("id       : " + id);
+            Console.WriteLine("Nik      : " + Nik);
+            Console.WriteLine("Nama     : " + nama);
+            Console.WriteLine("Gender   : " + gender);
+            Console.WriteLine("Course   : " + course);
+        }
+    }
+}
---
+++ VersionGit/Program.cs
@@ -0,0 +1,22 @@
+﻿using VersionGit;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dosen dosen = new Dosen();
+
+        Console.WriteLine("Deafult Dosen Pengampu:");
+        dosen.display();
+
+        Console.WriteLine("\nMasukkan Nama dan NIK Dosen Baru:");
+        dosen.attDosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+        dosen.display();
+
+        Console.WriteLine("\nMasukkan Mata Kuliah Baru:");
+        dosen.addCourse(Console.ReadLine());
+        dosen.display();
+
+        Console.ReadLine();
+    }
+}
